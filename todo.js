bdd98709c1c6a3bf5c4f28654e2df46b159d726f
Jeremy Iglehart <jeremy@autoschematic.com>
2015-11-28 21:40:25 -0500

    created todo.html and .css

--- todo.js
+++ todo.js
@@ -1,3 +1,21 @@
 function TodoCtrl($scope) {
-  $scope.totalTodos = 4;
+    $scope.todos = [
+        {text:'Learn AngularJS', done:false},
+        {text:'Build an app', done:false}
+    ];
+
+    $scope.getTotalTodos = function () {
+        return $scope.todos.length;
+    };
+
+    $scope.clearCompleted = function () {
+        $scope.todos = _.filter($scope.todos, function(todo){
+            return !todo.done;
+        });
+    };
+
+    $scope.addTodo = function () {
+        $scope.todos.push({text:$scope.formTodoText, done:false});
+        $scope.formTodoText = '';
+    };
 }
